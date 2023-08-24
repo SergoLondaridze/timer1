5a47ec2e0451bf1a9a1f626cac2322022ce75011
SergoLondaridze <sergo.londaridze@gmail.com>
2023-08-24 21:29:42 +0400

    add html ans css file

--- main.js
+++ main.js
@@ -11,11 +11,43 @@ window.addEventListener("DOMContentLoaded", () => {
     let seconds = Math.floor((t / 1000) % 60);
 
     return {
-      total: "t",
-      days: "days",
-      hours: "hours",
-      minutes: "minutes",
-      seconds: "seconds",
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
     };
   }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(endtime) {
+    const timer = document.querySelector(".container");
+    const days = timer.querySelector(".day");
+    const hour = timer.querySelector(".hour");
+    const minutes = timer.querySelector(".minutes");
+    const seconds = timer.querySelector(".second");
+    let timeInterval = setInterval(updateClock, 1000);
+    updateClock();
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hour.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock(deadline);
 });
